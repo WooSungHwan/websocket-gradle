c4b66317f7cbe8f25798b4184c74300a21a200d2
WooSungHwan <doqndnffo@gmail.com>
2020-09-14 12:09:20 +0900

    Initial commit

---
+++ src/main/java/demo/spring/websocketgradle/WebsocketGradleApplication.java
@@ -0,0 +1,13 @@
+package demo.spring.websocketgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketGradleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/demo/spring/websocketgradle/WebsocketGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.spring.websocketgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketGradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
